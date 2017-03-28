de8b067b5ff101b5d85feac66959f3569237fb86
Kevin Lu <luhkevin@gmail.com>
2017-03-28 01:38:04 -0700

    release 1.1.3

---
+++ docker/install-netsil-collectors.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ -z "${NETSIL_HOST}" ] ; then
+    echo "Exiting because NETSIL_HOST variable not defined"
+    exit 1
+fi
+
+docker run -td \
+       --name=netsil_collectors \
+       --net=host \
+       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       -v /proc/:/host/proc/:ro \
+       -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
+       --cap-add=NET_RAW \
+       --cap-add=NET_ADMIN \
+       -e NETSIL_SP_HOST=${NETSIL_HOST} \
+       -e NETSIL_TRAFFIC_PORT=2003 \
+       -e NETSIL_INFRA_PORT=2001 \
+       -e DEPLOY_ENV="docker" \
+       netsil/collectors:stable-1.1.3
---
+++ docker/install-netsil.sh
@@ -0,0 +1,20 @@
+docker run -td \
+    -v /opt/netsil/lite/ceph/conf:/etc/ceph \
+    -v /opt/netsil/lite/ceph/data:/var/lib/ceph-data \
+    -v /opt/netsil/lite/druid/realtime-segments:/opt/netsil/druid/realtime-segments \
+    -v /opt/netsil/lite/druid/indexCache:/var/tmp/druid/indexCache \
+    -v /opt/netsil/lite/elasticsearch/data:/usr/share/elasticsearch/data \
+    -v /opt/netsil/lite/kafka/kafka-log-dir:/opt/netsil/kafka/kafka-log-dir \
+    -v /opt/netsil/lite/license-manager/licenses:/opt/netsil/license-manager/licenses \
+    -v /opt/netsil/lite/mysql/data:/var/lib/mysql \
+    -v /opt/netsil/lite/redis:/var/lib/redis \
+    -v /opt/netsil/lite/zookeeper/data:/opt/netsil/zookeeper/data \
+    -p 80:80 \
+    -p 443:443 \
+    -p 2001:2001 \
+    -p 2003:2003 \
+    -p 2003:2003/udp \
+    -p 5005:5005 \
+    --log-driver=none \
+    netsil/netsil:stable-1.1.3 \
+    /root/startup.sh
