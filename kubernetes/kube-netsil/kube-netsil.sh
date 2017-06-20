cace96dda4052cb2b12d46985a3cc4adf5b46661
Harshal Shah <harshal@infracloud.io>
2017-06-20 17:09:00 +0530

    Removing yaml files and adding heredoc

--- kubernetes/kube-netsil/kube-netsil.sh
+++ kubernetes/kube-netsil/kube-netsil.sh
@@ -55,12 +55,103 @@ checkns(){
 }
 
 installAOCServer(){
-	cp -f aoc-server-deployment.yaml.tpl aoc-server-deployment.yaml;
-
-    sed -i '.orig' "s/#AOC_SERVER#/${AOC_Server}/g" aoc-server-deployment.yaml
-    # Delete deployment if it exists already
+	
+	# Delete deployment if it exists already
     kubectl get deployment netsil --namespace netsil > /dev/null 2>&1 && kubectl delete deployment netsil --namespace netsil
-    
+    # Generate AOC Server deployment
+    cat <<EOF>aoc-server-deployment.yaml
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  namespace: netsil
+  name: netsil
+spec:
+  replicas: 1
+  template:
+    metadata:
+      name: stream-processor
+      labels:
+        app: netsil
+        component: netsil
+        version: stable
+    spec:
+      containers:
+      - name: netsil
+        image: netsil/netsil:stable-1.4.4
+        command:
+        - /root/startup.sh
+        ports:
+        - containerPort: 80
+        - containerPort: 443
+        - containerPort: 2001
+        - containerPort: 2003
+        - containerPort: 2003
+          protocol: UDP
+        volumeMounts:
+        - name: alerts
+          mountPath: /var/lib/netsil/alerts/data
+        - name: ceph-conf
+          mountPath: /etc/ceph
+        - name: ceph-data
+          mountPath: /var/lib/ceph-data
+        - name: druid-index-cache
+          mountPath: /var/tmp/druid/indexCache
+        - name: druid-realtime-segments
+          mountPath: /var/lib/netsil/druid/realtime-segments
+        - name: elasticsearch
+          mountPath: /usr/share/elasticsearch/data
+        - name: kafka-logs
+          mountPath: /var/lib/netsil/kafka/kafka-log-dir
+        - name: licenses
+          mountPath: /var/lib/netsil/lite/license-manager/licenses
+        - name: mysql
+          mountPath: /var/lib/mysql
+        - name: redis
+          mountPath: /var/lib/redis
+        - name: user-persistence
+          mountPath: /var/lib/netsil/user-persistence/data
+        - name: zookeeper
+          mountPath: /var/lib/netsil/zookeeper/data
+      volumes:
+      - name: alerts
+        hostPath:
+          path: /var/lib/netsil/lite/alerts/data
+      - name: ceph-conf
+        hostPath:
+          path: /var/lib/netsil/lite/ceph/conf
+      - name: ceph-data
+        hostPath:
+          path: /var/lib/netsil/lite/ceph/data
+      - name: druid-index-cache
+        hostPath:
+          path: /var/tmp/druid/indexCache
+      - name: druid-realtime-segments
+        hostPath:
+          path: /var/lib/netsil/lite/druid/realtime-segments
+      - name: elasticsearch
+        hostPath:
+          path: /var/lib/netsil/lite/elasticsearch/data
+      - name: licenses
+        hostPath:
+          path: /var/lib/netsil/lite/license-manager/licenses
+      - name: kafka-logs
+        hostPath:
+          path: /var/lib/netsil/lite/kafka/kafka-log-dir
+      - name: mysql
+        hostPath:
+          path: /var/lib/netsil/lite/mysql/data
+      - name: redis
+        hostPath:
+          path: /var/lib/netsil/lite/redis
+      - name: user-persistence
+        hostPath:
+          path: /var/lib/netsil/lite/user-persistence/data
+      - name: zookeeper
+        hostPath:
+          path: /var/lib/netsil/lite/zookeeper/data
+      nodeSelector:
+        kubernetes.io/hostname: ${AOC_Server}
+EOF
     kubectl create -f aoc-server-deployment.yaml
     #
     # Check if AOC server is UP
@@ -84,8 +175,50 @@ installAOCServer(){
     fi
     # Install a service to expose AOC
     echo "INFO : Creating netsil service if it does not exist already";
+    cat <<EOF>netsil-svc.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  namespace: netsil
+  name: netsil
+spec:
+  selector:
+    app: netsil
+    component: netsil
+  ports:
+    - name: http
+      port: 80
+      nodePort: 31000
+    - name: https
+      port: 443
+      nodePort: 31443
+    - name: port2001
+      port: 2001
+    - name: port2003
+      port: 2003
+    - name: port2003udp
+      port: 2003
+      protocol: UDP
+  type: NodePort
+EOF
     kubectl get svc netsil --namespace netsil > /dev/null 2>&1 || kubectl create -f netsil-svc.yaml
     if [[ "${SERVICE_TYPE}" == "LoadBalancer" ]]; then
+        # Generate LB service file
+        cat <<EOF>netsil-svc-lb.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  namespace: netsil
+  name: netsil-lb
+spec:
+  selector:
+    app: netsil
+    component: netsil
+  ports:
+    - name: http
+      port: 80
+  type: LoadBalancer
+EOF
         kubectl create -f netsil-svc-lb.yaml
     fi
 }
@@ -93,6 +226,62 @@ installAOCServer(){
 installAOCCollectors()
 {
     echo "INFO : Installing netsil collectors via DaemonSet"
+    cat <<EOF>aoc-collectors-daemonset.yaml
+apiVersion: extensions/v1beta1
+kind: DaemonSet
+metadata:
+  namespace: netsil
+  name: collector
+  labels:
+    app: netsil
+    component: collector
+spec:
+  template:
+    metadata:
+      labels:
+        app: netsil
+        component: collector
+    spec:
+      hostNetwork: true
+      containers:
+      - name: collector
+        image: netsil/collectors:stable-1.3.1
+        command: ["/bin/bash","-c","while true ; do NETSIL_SP_HOST=\$NETSIL_SERVICE_HOST /opt/netsil/collectors/start.sh ; echo Exiting, possibly to upgrade ; sleep 5 ; done"]
+        securityContext:
+          capabilities:
+            add:
+            - NET_RAW
+            - NET_ADMIN
+        env:
+        - name: NETSIL_TRAFFIC_PORT
+          value: '2003'
+        - name: NETSIL_INFRA_PORT
+          value: '2001'
+        - name: DEPLOY_ENV
+          value: 'docker'
+        - name: SAMPLINGRATE
+          value: '100'
+        volumeMounts:
+        - name: cgroup
+          mountPath: /host/sys/fs/cgroup/
+          readOnly: true
+        - name: proc
+          mountPath: /host/proc/
+          readOnly: true
+        - name: docker-sock
+          mountPath: /var/run/docker.sock
+          readOnly: true
+      volumes:
+      - name: cgroup
+        hostPath:
+          path: /sys/fs/cgroup/
+      - name: proc
+        hostPath:
+          path: /proc/
+      - name: docker-sock
+        hostPath:
+          path: /var/run/docker.sock
+EOF
     kubectl get daemonset collector -n netsil > /dev/null 2>&1 || kubectl create -f aoc-collectors-daemonset.yaml
     for n in {1..5}
     do
@@ -132,7 +321,9 @@ displayInfo(){
 	if [[ "${SERVICE_TYPE}" == "LoadBalancer" ]]; then
 	    echo "INFO : AOC server is available on Load Balancer IP $LB_IP";
 	else
-	    echo "INFO : AOC server is avaialble on Port 31000 of any node of this cluster";
+	    node_ips=$(kubectl get nodes --output=jsonpath={.items..status.addresses[1].address})
+	    echo "INFO : Node IPs ${node_ips}";
+	    echo "INFO : AOC server is avaialble on Port 31000 of above IPs";
 	    echo "INFO : Please ensure this port is open in firewall";
 	fi
 
@@ -141,11 +332,10 @@ displayInfo(){
 }
 
 main(){
-	clear;
+    clear;
     echo "=========== Cluster Information =================";
     kubectl cluster-info
-    echo "=================================================";
-    echo "PRE-REQS : "
+    echo "PRE-REQS :";
     echo "AOC server needs at least one node with min 4 CPU and 15GB RAM";
     echo "ALL nodes need internet connectivity to pull images from Docker Hub"
     echo "PROMPT : Please confirm that you want to install AOC on above cluster (y/n)[y]:";
@@ -157,6 +347,7 @@ main(){
     #
     # Handling Service type by prompting user
     #
+    echo "=============================================================";
     echo "INFO : This automation does not support ingress controller.";
     echo "PROMPT : Default Service type is NodePort. Type yes if you want a LoadBalancer instead (y/n)[n]:";
     read choice;
@@ -171,14 +362,13 @@ main(){
     checkCPURAM
     if [[ "${AOC_Server}" == "" ]]; then
         echo "ERROR: Could not find a node which has at least 4 CPU and 15GB RAM. Below are Nodes with their CPU and RAM values:";
-        echo ""
         checkCPURAM print
         exit 1
     fi
     checkns
     installAOCServer
-	installAOCCollectors
-	displayInfo
+    installAOCCollectors
+    displayInfo
 }
 
 main
