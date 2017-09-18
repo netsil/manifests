c10d407fb7708c0eb846adc4484a90446f9e7cbf
Kevin Lu <luhkevin@gmail.com>
2017-09-18 02:40:41 -0700

    1.7.0 and org id

--- docker/install-collectors.sh
+++ docker/install-collectors.sh
@@ -11,4 +11,6 @@ docker run -td \
        -e DEPLOY_ENV="docker" \
        -e SD_BACKEND="docker" \
        -e NETSIL_SP_HOST=${NETSIL_HOST} \
-       netsil/collectors:stable-1.6.2
+       -e NETSIL_SP_HOST=${NETSIL_HOST} \
+       -e NETSIL_ORGANIZATION_ID=${NETSIL_ORGANIZATION_ID} \
+       netsil/collectors:stable-1.7.0
--- docker/install-netsil.sh
+++ docker/install-netsil.sh
@@ -17,5 +17,5 @@ docker run -td \
     -p 2003:2003 \
     -p 2003:2003/udp \
     --log-driver=none \
-    netsil/netsil:stable-1.6.2 \
+    netsil/netsil:stable-1.7.0 \
     /root/startup.sh
